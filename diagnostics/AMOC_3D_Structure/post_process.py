3f3083f60018ff5b7d2933ee169aa627cee5273a
Thomas Jackson <tom.jackson314@gmail.com>
2020-09-01 01:49:45 -0400

    Fixes in AMOC execute_ncl_calculate()
    NOAA-GFDL/MDTF-diagnostics#20

--- diagnostics/AMOC_3D_Structure/post_process.py
+++ diagnostics/AMOC_3D_Structure/post_process.py
@@ -8,15 +8,16 @@ def execute_ncl_calculate(nclPlotFile):
     nclPlotFile (string) - full path to ncl plotting file name
     """
     import subprocess
+    import time
     # check if the nclPlotFile exists -
     # don't exit if it does not exists just print a warning.
     try:
+        print('Start NCL routine {0}'.format(nclPlotFile))
         pipe = subprocess.Popen(['ncl -Q {0}'.format(nclPlotFile)], shell=True, stdout=subprocess.PIPE)
         output = pipe.communicate()[0]
-        print('NCL routine {0} \n {1}'.format(nclPlotFile,output))
         while pipe.poll() is None:
             time.sleep(0.5)
+        print('End NCL routine {0}: \n {1}'.format(nclPlotFile,output))
     except OSError as e:
         print('WARNING',e.errno,e.strerror)
 
-    return 0
