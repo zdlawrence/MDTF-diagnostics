efc51c00412bca52ae30293fa60ff7fc4aa1f890
tsjackson-noaa <thomas.jackson@noaa.gov>
2020-10-08 15:51:24 -0400

    Don't retain MDTF output; just run verify_links

--- tests/gfdl_ci.sh
+++ tests/gfdl_ci.sh
@@ -16,6 +16,10 @@ set -xv
 REPO_NAME="MDTF-diagnostics"
 # use conda envs, data from MDTeam installation
 MDTEAM_MDTF="/home/mdteam/DET/analysis/mdtf/${REPO_NAME}"
+# path to env module command on analysis; env modules defined as a shell alias
+MODULECMD="/usr/local/Modules/default/bin/modulecmd"
+# output directory of MDTF script
+OUTPUT_DIR="${TMPDIR}/wkdir"
 
 DEFAULT_POD="all"
 DEFAULT_BRANCH="feature/gfdl-data"
@@ -50,7 +54,8 @@ if [ -z "$BRANCH" ]; then
     BRANCH="$DEFAULT_BRANCH"
 fi
 
-# module load git
+# make sure we have git
+eval $( "$MODULECMD" bash load git )
 
 # checkout requested branch into $TMPDIR
 cd $TMPDIR
@@ -70,12 +75,36 @@ if [[ "$POD" != "$DEFAULT_POD" && ! -d "diagnostics/${POD}" ]]; then
     POD="$DEFAULT_POD"
 fi
 
+# activate MDTF framework's conda environment from the MDTeam install
 if [ ! -d "$MDTEAM_MDTF" ]; then
     echo "ERROR: can't find MDTeam install at ${MDTEAM_MDTF}" 1>&2
     exit 1
 fi
 source "${MDTEAM_MDTF}/src/conda/conda_init.sh" "/home/mdteam/anaconda"
 conda activate "${MDTEAM_MDTF}/envs/_MDTF_base"
+mkdir -p "$OUTPUT_DIR" # not strictly necessary
 
 # run script
-/usr/bin/env python -m src.mdtf_gfdl -f "${MDTEAM_MDTF}/tests/gfdl_ci_config.jsonc" --pods "$POD" -- 
+/usr/bin/env python -m src.mdtf_gfdl -f "${MDTEAM_MDTF}/tests/gfdl_ci_config.jsonc" -o "$OUTPUT_DIR" --pods "$POD" -- 
+
+# test to see if $OUTPUT_DIR has only one subdirectory and no other content
+if [ "$( find "$OUTPUT_DIR" -maxdepth 1 -mindepth 1 -printf %y )" = "d" ]; then
+    RESULTS_DIR=$( find "$OUTPUT_DIR" -maxdepth 1 -mindepth 1 )
+    # run verify_links to determine if all plots created (interpret that as success)
+    if [ ! -x ./src/verify_links.py ]; then
+        echo "Couldn't find verify_links.py."
+        exit 1
+    fi
+    ./src/verify_links.py -v "$RESULTS_DIR" || error_code=$?
+    if [ "${error_code}" -eq 0 ]; then
+        echo "All links found; test completed successfully."
+        exit 0
+    else
+        echo "Error returned by verify_links; test failed."
+        exit 1
+    fi
+else
+    echo "Found unexpected content in $OUTPUT_DIR:"
+    ls "$OUTPUT_DIR"
+    exit 1
+fi
